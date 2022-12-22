c4350d4c607c3ce5651631d6e19d5ea611d39c47
Manolo Martínez <manolo@austrohungaro.com>
2022-12-22 20:20:55 +0100

    A working version of all_paths()

--- 2022/day16/day16.py
+++ 2022/day16/day16.py
@@ -1,6 +1,7 @@
 from itertools import product, filterfalse
 from collections import OrderedDict, defaultdict
 from bisect import insort
+from copy import deepcopy
 
 # Solutions:
 # result1("input.txt")
@@ -26,10 +27,6 @@ def create_cave(lines):
         cave[0][rate[0]] = rate[1]
         cave[1][tunnels[0]] = {(tunnel, 1) for tunnel in tunnels[1]}
     return cave
-
-
-## I need to parse the input more intelligently: rate 0 valves should just be
-## bypassed, and the time needed to reach the following valve be 2 minutes
 
 
 def optimize_cave(cave):
@@ -67,98 +64,39 @@ def release(rates, opened):
     return total
 
 
-def navigate(cave):
-    rates, tunnels = cave
-    runs = [[set(["AA"]), "AA", [0], defaultdict(bool), 0]]
-    # a set of tunnels visited so far, the last visited, a list of pressure released at each step,
-    # a list of open valves, and a counter of minutes passed
-    new_runs = []
-    max_release = 0
-    # import pdb; pdb.set_trace()
-    while len(runs) > 0:
-        for run in runs:
-            visited, last, released, opened, minutes = run
-            # print(visited)
-            for tunnel in tunnels[last]:
-                new_visited = visited.copy()
-                new_visited.add(tunnel[0])
-                new_minutes = minutes + tunnel[1]
-                new_released = released + [released[-1] + release(rates, opened) * tunnel[1]]
-                candidate = [new_visited, tunnel[0], new_released, opened, new_minutes]
-                if not worse(candidate, new_runs):
-                    if new_minutes >= 30:
-                        if new_released[-1] > max_release and new_minutes == 30:
-                            max_release = new_released[-1]
-                            print(new_released)
+def all_paths(cave):
+    """
+    Calculate time from every tunnel to every other tunnel
+    """
+    _, original_routes = cave
+    optimized_routes = deepcopy(original_routes)
+    original_routes = {}
+    while original_routes != optimized_routes:
+        original_routes = deepcopy(optimized_routes)
+        for tunnel in original_routes:
+            for following1 in original_routes[tunnel]:
+                for following2 in original_routes[following1[0]]:
+                    if (
+                        not any(
+                            following2[0] == following[0]
+                            for following in original_routes[tunnel]
+                        )
+                        and following2[0] != tunnel
+                    ):
+                        optimized_routes[tunnel].add(
+                            (following2[0], following1[1] + following2[1])
+                        )
                     else:
-                        prev_runs = new_runs.copy()
-                        new_runs = [candidate]
-                        for run in prev_runs:
-                            if not vet2(candidate, run):
-                                new_runs.append(run)
-                if rates[tunnel[0]] and not opened[tunnel[0]]:
-                    new_opened = opened.copy()
-                    new_opened[tunnel[0]] = True
-                    new_minutes += 1
-                    new_released = new_released + [
-                        new_released[-1] + release(rates, opened)
-                    ]
-                    candidate = [
-                        new_visited,
-                        tunnel[0],
-                        new_released,
-                        new_opened,
-                        new_minutes,
-                    ]
-                if not worse(candidate, new_runs):
-                    if new_minutes >= 30:
-                        if new_released[-1] > max_release and new_minutes == 30:
-                            max_release = new_released[-1]
-                            print(new_released)
-                    else:
-                        prev_runs = new_runs.copy()
-                        new_runs = [candidate]
-                        for run in prev_runs:
-                            if not vet2(candidate, run):
-                                new_runs.append(run)
-        runs = new_runs
-        print(len(runs))
-        new_runs = []
-    return max_release
-
-
-def worse(candidate, runs):
-    visited, last, released, opened, minutes = candidate
-    for run in runs:
-        if (
-            visited.issubset(run[0])
-            and last == run[1]
-            and all(
-                [
-                    cand_opened <= run_opened
-                    for cand_opened, run_opened in zip(opened.values(), run[3].values())
-                ]
-            )
-            and released[-1] <= run[2][-1]
-            and minutes >= run[4]
-        ):
-            return True
-    return False
-
-
-def vet2(new, old):
-    visited, last, released, opened, minutes = new
-    if (
-        old[0].issubset(visited)
-        and last == old[1]
-        and all(
-            [
-                old_opened <= new_opened
-                for old_opened, new_opened in zip(old[3].values(), opened.values())
-            ]
-        )
-        and old[2][-1] <= released[-1]
-        and old[4] >= minutes
-    ):
-        return True
-    return False
+                        for following in original_routes[tunnel]:
+                            if (
+                                following[0] == following2[0]
+                                and following[1] > following1[1] + following2[1]
+                            ):
+                                try:
+                                    optimized_routes[tunnel].remove(following)
+                                    optimized_routes[tunnel].add(
+                                        (following[0], following1[1] + following2[1])
+                                    )
+                                except KeyError:
+                                    pass
+    return optimized_routes
